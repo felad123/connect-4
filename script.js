3d6f53710a025b52a73675328699410cbc122037
Adrian <xxxdonkeygoatxxx@gmail.com>
2019-03-21 12:08:56 +1100

    gameEnd winning lines down

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 var board = [];
+var move = 0;
 function newGame() {
   board = [];
   for (i = 1; i < 50; i++) {
     if (i % 7 == 0) {
       board.push(0);
-    } else {
+    } 
+    else {
       board.push("E");
+    }
+  }
+}
+function gameEnd() {
+  //winning lines downwards
+  if (board[7*move+6]>=4) {
+    if (board[7*move+board[7*move+6]-1]==board[7*move+board[7*move+6]-2]==board[7*move+board[7*move+6]-3]==board[7*move+board[7*move+6]-4]) {
+      return(1);
     }
   }
 }
