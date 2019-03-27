5a126660ee2d1e967829b7a42902c1f1a34f6a34
Adrian <xxxdonkeygoatxxx@gmail.com>
2019-03-28 09:25:02 +1100

    gameEnd() winning lines sideways

--- script.js
+++ script.js
@@ -15,7 +15,32 @@ function gameEnd() {
   //winning lines downwards
   if (board[7*move+6]>=4) {
     if (board[7*move+board[7*move+6]-1]==board[7*move+board[7*move+6]-2]==board[7*move+board[7*move+6]-3]==board[7*move+board[7*move+6]-4]) {
-      return(1);
+      return true;
     }
+  }
+  var i=move-1
+  var sameC=1
+  //winning lines sideways
+  while (i>=0) {
+    if (board[7*i+board[7*move+6]-1]==board[7*move+board[7*move+6]-1]){
+      sameC++;
+    }
+    else {
+      break;
+    } 
+    i--
+  }
+  i=move+1
+  while (i<=6) {
+    if (board[7*i+board[7*move+6]-1]==board[7*move+board[7*move+6]-1]) {
+      sameC++;
+    }
+    else {
+      break;
+    }
+    i++
+  }
+  if (sameC>=4) {
+    return true;
   }
 }
