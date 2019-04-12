52f21b94199c982e6cd59f0fc29d856a84c4e7a8
Adrian <xxxdonkeygoatxxx@gmail.com>
2019-04-12 12:39:49 +1000

    animation fix

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 var board = [];
 var move = 0;
 var movecount = 0;
+var speed = 200;
 function newGame() {
+  movecount = 0;
   board = [];
   for (i = 1; i < 50; i++) {
-    if (i % 7 == 0) {
+    if (i%7 == 0) {
       board.push(0);
     } 
     else {
       board.push("E");
-      document.getElementById(String(i-1)).src = "https://i.imgur.com/aPGAmz4.png"
+      document.getElementById(String(i-1)).src = "https://i.imgur.com/esKL89z.png"
     }
   }
 }
@@ -20,22 +22,22 @@ function moveChoiceOff(i) {
   document.getElementById("top"+String(i)).src = "https://i.imgur.com/OyC93RA.png"
 }
 function gameState() {
-  var sameC=1;
+  var sameC = 1;
   //winning lines downwards
-  if (board[7*move+6]>=4) {
-    for (i=2;i<=4;i++) {
+  if (board[7*move+6] >= 4) {
+    for (i = 2; i <= 4; i++) {
       if (board[7*move+board[7*move+6]-1]==board[7*move+board[7*move+6]-i]) {
         sameC++
       }
     }
-    if (sameC==4) {
+    if (sameC == 4) {
       return "win"
     }
   }
-  var i=move-1;
-  sameC=1;
+  var i = move-1;
+  sameC = 1;
   //winning lines sideways
-  while (i>=0) {
+  while (i >= 0) {
     if (board[7*i+board[7*move+6]-1]==board[7*move+board[7*move+6]-1]){
       sameC++;
     }
@@ -44,8 +46,8 @@ function gameState() {
     } 
     i--;
   }
-  i=move+1
-  while (i<=6) {
+  i = move+1
+  while (i <= 6) {
     if (board[7*i+board[7*move+6]-1]==board[7*move+board[7*move+6]-1]) {
       sameC++;
     }
@@ -54,13 +56,13 @@ function gameState() {
     }
     i++;
   }
-  if (sameC>=4) {
+  if (sameC >= 4) {
     return "win"
   }
-  sameC=1 
+  sameC = 1 
   //winning lines diagonal bottom left to top right
-  if (Math.min(move,board[7*move+6]-1)>=1) {
-    for (i=1;i<Math.min(move,board[7*move+6]-1)+1;i++) {
+  if (Math.min(move,board[7*move+6]-1) >= 1) {
+    for (i = 1; i < Math.min(move,board[7*move+6]-1)+1; i++) {
       if (board[7*move+board[7*move+6]-1]==board[7*(move-i)+board[7*move+6]-1-i]) {
         sameC++
       }
@@ -69,8 +71,8 @@ function gameState() {
       }
     }
   }
-  if (Math.min(6-move,6-board[7*move+6])>=1) {
-    for (i=1;i<Math.min(6-move,6-board[7*move+6])+1;i++) {
+  if (Math.min(6-move,6-board[7*move+6]) >= 1) {
+    for (i = 1; i < Math.min(6-move,6-board[7*move+6])+1; i++) {
       if (board[7*move+board[7*move+6]-1]==board[7*(move+i)+board[7*move+6]-1+i]) {
         sameC++
       }
@@ -79,14 +81,14 @@ function gameState() {
       }
     }
   }
-  if (sameC>=4) {
+  if (sameC >= 4) {
     return "win"
   }
-  sameC=1
+  sameC = 1
   //winning lines diagonal bottom right to top left
-  if (Math.min(6-move,board[7*move+6]-1)>=1) {
-    for (i=1;i<Math.min(6-move,board[7*move+6])+1;i++) {
-      if (board[7*move+board[7*move+6]-1]==board[7*(move+i)+board[7*move+6]-1-i]) {
+  if (Math.min(6-move,board[7*move+6]-1) >= 1) {
+    for (i = 1; i < Math.min(6-move,board[7*move+6])+1; i++) {
+      if (board[7*move+board[7*move+6]-1] == board[7*(move+i)+board[7*move+6]-1-i]) {
         sameC++
       }
       else {
@@ -94,8 +96,8 @@ function gameState() {
       }
     }
   }
-  if (Math.min(move,6-board[7*move+6]-1)>=1) {
-    for (i=1;i<Math.min(move,6-board[7*move+6]-1)+1;i++) {
+  if (Math.min(move,6-board[7*move+6]-1) >= 1) {
+    for (i = 1; i < Math.min(move,6-board[7*move+6]-1)+1; i++) {
       if (board[7*move+board[7*move+6]-1]==board[7*(move-i)+board[7*move+6]-1+i]) {
         sameC++
       }
@@ -104,41 +106,46 @@ function gameState() {
       }
     }
   }
-  if (sameC>=4) {
+  if (sameC >= 4) {
     return "win"
   }
-  if (board.indexOf("E")==-1) {
+  if (board.indexOf("E") == -1) {
     return "draw"
   }
 }
-function doMove() {
-  if (board[7*move+6]!=6) {
+function doMove(i) {
+  move = i;
+  if (board[7*move+6] != 6) {
+    animation()
+    setTimeout(showCell, 100000*((5-board[7*move+6])/speed))
+  function showCell() {
     board[7*move+board[7*move+6]] = ["R","Y"][movecount%2]
     document.getElementById(String(7*move+board[7*move+6])).src = ["https://i.imgur.com/lpCPbnt.png","https://i.imgur.com/km8rY9I.png"][movecount%2]
     board[7*move+6]++
-    if (gameState()=="win") {
+    if (gameState() == "win") {
       console.log(["R","Y"][(movecount)%2] + " won")
     }
     movecount++
-    moveChoiceOn(move)
+  }
   }
 }
 
 
 
-function animationTest() {
-  var elem = document.getElementById("rCell");   
-  var pos = 0;
-  var id = setInterval(frame, 10);
+function animation() {
+  var elem = document.getElementById(["rMoving","yMoving"][movecount%2]);
+  elem.style.left = (document.getElementById("top0").getBoundingClientRect().left + 100*move + window.scrollX) + "px"
+  var height = document.getElementById("top0").getBoundingClientRect().top + window.scrollY
+  var i = 0;
+  var id = setInterval(frame, 1/speed);
   function frame() {
-    if (pos == 100) {
-      clearInterval(id);
-    } else {
-      pos++; 
-      elem.style.top = pos + 'px'; 
-      elem.style.left = pos + 'px'; 
+    if (i >= 100*(6-board[7*move+6])) {
+      clearInterval(id)
+    }
+    else {
+      i++
+      height++
+      elem.style.top = height + "px"
     }
   }
 }
-
-
